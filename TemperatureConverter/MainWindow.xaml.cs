7eaf1032ec532b0af0721d376f84e23da07732d6
akcinar.omer <akcinardev@gmail.com>
2024-07-22 22:23:17 +0200

    UI arranged

--- TemperatureConverter/MainWindow.xaml.cs
+++ TemperatureConverter/MainWindow.xaml.cs
@@ -11,14 +11,16 @@ using System.Windows.Shapes;
 
 namespace TemperatureConverter
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void ConvertButton_Clicked(object sender, RoutedEventArgs e)
+		{
+			// TODO
 		}
 	}
 }
